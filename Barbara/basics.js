4bf23c80c988af151caf3de79085d96bf2c08812
BarbaraGaluppo <barbaragaluppo@gmail.com>
2021-06-24 11:05:00 +0200

    Adds Basics

---
+++ Barbara/basics.js
@@ -0,0 +1,18 @@
+
+
+// BASIC 1
+
+
+function crystalGazer(a, b, c, d) {
+    return "You will be a' ${a} in ${b} and married to ${c} with ${d} children.";
+
+}
+console.log(crystalGazer ("teacher","Moscow", "John", "3"))
+
+
+// BASIC 2
+
+function ageCalculator(){
+
+    
+}
